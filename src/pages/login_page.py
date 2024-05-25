5ff3b507ae63d97273a9e6ca3a726635d3a6b1d8
Pascal Nitcheu <mbassistance@Pascals-MacBook-Pro.local>
2024-05-25 22:16:57 +0100

    removing actions where it was not needed and also adjusting the navigate back

--- src/pages/login_page.py
+++ src/pages/login_page.py
@@ -1,12 +1,24 @@
-from src.pages.base_page import BasePage
-from src.utils.helpers import click_button, enter_text, send_text, wait_for_toast_message, navigate_back
-from src.utils.constants import (
+
+import sys
+import os
+
+# Add the src directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+import time 
+
+from pages.base_page import BasePage
+from utils.helpers import click_button, enter_text, send_text, wait_for_toast_message, navigate_back
+from utils.constants import (
     LOG_IN_BUTTON, EMAIL_INPUT, PASSWORD_INPUT, CONFIRM_PASSWORD_INPUT, NAME_INPUT, 
-    EMAIL_ALREADY_EXISTS_TOAST, WRONG_EMAIL_OR_PASSWORD_TOAST, LOGIN_PAGE_VIEW_GROUP, 
+    EMAIL_ALREADY_EXISTS_TOAST, WRONG_EMAIL_OR_PASSWORD_TOAST, LOGIN_PAGE_VIEW_GROUP, ERROR,
     PASSWORD_VIEW_GROUP, SIGNUP_VIEW_GROUP, FINALIZE_SIGNUP_VIEW_GROUP, 
     BACK_BUTTON_PATH_VIEW, FINALIZE_LOGIN_VIEW_GROUP
 )
 from selenium.webdriver.common.by import By
+
+from appium.webdriver.common.appiumby import AppiumBy
+
+
 import logging
 
 class LoginPage(BasePage):
@@ -14,16 +26,17 @@ class LoginPage(BasePage):
         """Logs in using the provided email and password."""
         logging.info("Attempting to log in")
         try:
-            click_button(self.driver, By.ANDROID_UIAUTOMATOR, LOG_IN_BUTTON)
-            click_button(self.driver, By.ANDROID_UIAUTOMATOR, LOGIN_PAGE_VIEW_GROUP)
-            enter_text(self.driver, (By.ANDROID_UIAUTOMATOR, EMAIL_INPUT), email)
-            enter_text(self.driver, (By.ANDROID_UIAUTOMATOR, PASSWORD_INPUT), password)
+            click_button(self.driver, AppiumBy.ANDROID_UIAUTOMATOR, LOG_IN_BUTTON)
+            click_button(self.driver, AppiumBy.ANDROID_UIAUTOMATOR, LOGIN_PAGE_VIEW_GROUP)
+            enter_text(self.driver, (AppiumBy.ANDROID_UIAUTOMATOR, EMAIL_INPUT), email)
+            enter_text(self.driver, (AppiumBy.ANDROID_UIAUTOMATOR, PASSWORD_INPUT), password)
+            click_button(self.driver, AppiumBy.ANDROID_UIAUTOMATOR, FINALIZE_LOGIN_VIEW_GROUP) 
 
-            if not wait_for_toast_message(self.driver, (By.ANDROID_UIAUTOMATOR, WRONG_EMAIL_OR_PASSWORD_TOAST)):
-                logging.info("Log In successful!")
-                click_button(self.driver, By.ANDROID_UIAUTOMATOR, FINALIZE_LOGIN_VIEW_GROUP)
-            else:
+            if  wait_for_toast_message(self.driver, (AppiumBy.ANDROID_UIAUTOMATOR, WRONG_EMAIL_OR_PASSWORD_TOAST)):
                 logging.warning("Verify either the email or the password or sign up.")
+            else:
+                logging.info("Log In successful!")
+                time.sleep(30)
         except Exception as e:
             logging.error(f"Error logging in: {e}")
 
@@ -31,20 +44,26 @@ class LoginPage(BasePage):
         """Signs up a new user with the provided email, password, and name."""
         logging.info("Attempting to sign up")
         try:
-            click_button(self.driver, By.ANDROID_UIAUTOMATOR, LOGIN_PAGE_VIEW_GROUP)
-            enter_text(self.driver, (By.ANDROID_UIAUTOMATOR, EMAIL_INPUT), email)
-            click_button(self.driver, By.ANDROID_UIAUTOMATOR, PASSWORD_VIEW_GROUP)
-
-            if not wait_for_toast_message(self.driver, (By.ANDROID_UIAUTOMATOR, EMAIL_ALREADY_EXISTS_TOAST)):
-                enter_text(self.driver, (By.ANDROID_UIAUTOMATOR, PASSWORD_INPUT), password)
-                enter_text(self.driver, (By.ANDROID_UIAUTOMATOR, CONFIRM_PASSWORD_INPUT), password)
-                click_button(self.driver, By.ANDROID_UIAUTOMATOR, SIGNUP_VIEW_GROUP)
-                enter_text(self.driver, (By.ANDROID_UIAUTOMATOR, NAME_INPUT), name)
-                click_button(self.driver, By.ANDROID_UIAUTOMATOR, FINALIZE_SIGNUP_VIEW_GROUP)
-                time.sleep(10)
+            click_button(self.driver, AppiumBy.ANDROID_UIAUTOMATOR, LOGIN_PAGE_VIEW_GROUP)
+            enter_text(self.driver, (AppiumBy.ANDROID_UIAUTOMATOR, EMAIL_INPUT), email)
+            click_button(self.driver, AppiumBy.ANDROID_UIAUTOMATOR, PASSWORD_VIEW_GROUP)
+            email_exist = wait_for_toast_message(self.driver, (AppiumBy.ANDROID_UIAUTOMATOR, EMAIL_ALREADY_EXISTS_TOAST))
+            if not email_exist:
+                enter_text(self.driver, (AppiumBy.ANDROID_UIAUTOMATOR, PASSWORD_INPUT), password)
+                enter_text(self.driver, (AppiumBy.ANDROID_UIAUTOMATOR, CONFIRM_PASSWORD_INPUT), password)
+                click_button(self.driver, AppiumBy.ANDROID_UIAUTOMATOR, SIGNUP_VIEW_GROUP)
+                enter_text(self.driver, (AppiumBy.ANDROID_UIAUTOMATOR, NAME_INPUT), name)
+                print("finalizing it and not knowing why It does not work ")
+                click_button(self.driver, AppiumBy.ANDROID_UIAUTOMATOR, FINALIZE_SIGNUP_VIEW_GROUP)
+                error_auth = wait_for_toast_message(self.driver, (AppiumBy.ANDROID_UIAUTOMATOR, ERROR))
+                if error_auth:
+                    print("error signing up !!! bug !!!!!!!!!!!!!!")
+                time.sleep(30)
             else:
-                while not wait_for_toast_message(self.driver, (By.ANDROID_UIAUTOMATOR, LOG_IN_BUTTON)):
+                Loggin_in_exist = wait_for_toast_message(self.driver, (AppiumBy.ANDROID_UIAUTOMATOR, LOG_IN_BUTTON))
+                while not Loggin_in_exist:
                     navigate_back(self.driver)
+                    Loggin_in_exist = wait_for_toast_message(self.driver, (AppiumBy.ANDROID_UIAUTOMATOR, LOG_IN_BUTTON))
                 self.logging_in(email, password)
         except Exception as e:
             logging.error(f"Error signing up: {e}")
