c7eff0188e9fd28b8505b66518817e2e90db75cd
Pascal Nitcheu <nitcheupascal@gmail.com>
2024-05-28 12:14:12 +0100

    playing the coursesafter logging in

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import sys
 import os
 import logging
-
+import time 
 # Add the src directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 from drivers.driver import DriverManager
 from pages.login_page import LoginPage
+from pages.play_courses_page import PlayPage
+from utils.helpers  import close_app,reopen_app
+from utils.constants import (DISPLAY_HIGH_SCHOOL_COURSES,PLAY_HIGH_SCHOOL_COURSES)
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -14,8 +17,14 @@ if __name__ == "__main__":
     driver_manager = DriverManager()
     driver = driver_manager.get_driver()
     login_page = LoginPage(driver)
+    play_page = PlayPage(driver)
 
     try:
-        login_page.signing_up("12311434334mbouendeu@gmail.com", "P@ssw0rd","queueee") #, "nameee1245"
+        login_page.logging_in("nitcheupascal@gmail.com", "P@ssw0rd") #, "nameee1245"
+        close_app(driver)
+        reopen_app(driver)
+        time.sleep(3)
+        play_page.play_course_units(DISPLAY_HIGH_SCHOOL_COURSES,PLAY_HIGH_SCHOOL_COURSES)
+        time.sleep(10)
     finally:
         driver_manager.quit_driver()
